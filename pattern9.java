e823862f5a1d133a2429c7023a0a0a7812b9a3c0
rajefam <rajeswarireddy514@gmail.com>
2018-10-08 12:11:53 +0530

    pattern

---
+++ pattern9.java
@@ -0,0 +1,23 @@
+
+public class pattern9 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c=1,k=100;;
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=c;j++)
+			{
+				System.out.print(k);
+				k=k+100;
+			}
+			System.out.println();
+			c++;
+		}
+
+	}
+
+}
